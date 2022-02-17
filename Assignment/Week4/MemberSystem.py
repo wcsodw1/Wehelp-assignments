436867675172465b025c867915adb3d9c1336602
wcsodw1 <wcsodw1@gmail.com>
2022-02-17 10:30:40 +0800

    2022 Wehelp Assignment4 Update

--- Assignment/Week4/MemberSystem.py
+++ Assignment/Week4/MemberSystem.py
@@ -17,10 +17,13 @@ app = Flask(__name__, static_folder="static", static_url_path="/")
 #  1.3 設定session密鑰 : 密秘鑰匙
 app.secret_key = "Any string but secret"  # 設定 Session 的秘鑰
 # ========================================================================= #
-
+email_test = "test"
+password_test = 'test'
 
 # 2.路由設定 :
 #   2.1 首頁
+
+
 @app.route("/")
 def front_page():
     return render_template("front_page.html")
@@ -37,7 +40,7 @@ def signin():
     print("password : ", password)
 
     # A.成功登入 : 信箱/密碼驗證成功, 儲存驗證的Data(Email. Password)放入Session中
-    if email == "test" and password == "test":
+    if email == email_test and password == password_test:
         print("Enter")
         session["varify_email"] = email
         session["varify_password"] = password
@@ -57,10 +60,8 @@ def signin():
 # 2.3 會員路由 :
 @app.route("/member/")
 def member():
-    email_ = 'test'
-    password_ = 'test'
     # A.若信箱(帳號)及密碼存在資料中(驗證成功的意思), 導向會員頁面
-    if session["varify_email"] == email_ and session["varify_password"] == password_:
+    if session["varify_email"] == email_test and session["varify_password"] == password_test:
         return render_template("member.html")
 
     # B.若會員資料未符合, 就算設定/member路由, 仍強制導回首頁
@@ -73,15 +74,6 @@ def member():
 def error():
     msg = request.args.get("message", "")
     return render_template("error.html", message=msg)
-    # # A.失敗介面1 : 若輸入資料為空值, 在頁面中寫上請輸入帳號、密碼
-    # if session["varify_email_None"] == '' or session["varify_password_None"] == '':
-    #     print("None-")
-    #     message = request.args.get("msg", "請輸入帳號、密碼")
-    #     return render_template("error.html", message=message)
-
-    # # B.失敗介面2 : 若找不到相對應帳號密碼,且無空值, 則登入失敗, 並導向錯誤頁面
-    # message2 = request.args.get("msg", "帳號 、或密碼輸入錯誤")
-    # return render_template("error.html", message=message2)  # error = 失敗頁面
 
 
 # 2.5 signout(登出)功能網址 :
